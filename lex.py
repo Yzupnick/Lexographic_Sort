f9ed8692c53d0ef9d656a6b6258e07fe0938e952
Chanan Zupnick <chazup@gmail.com>
2015-12-06 11:09:46 -0500

    fixed bug related to words of different sizes

--- lex.py
+++ lex.py
@@ -1,2 +1,16 @@
-def lex_sort(words, order):
+def lex_sort(words, order, index = 0):
+    if len(words) <= 1:
+        return words
+    order_map = {char:index for index,char in enumerate(order)}
+    buckets = [ [] for char in order]
+    to_return = []
+    for word in words:
+        if index >= len(word):
+            to_return.append(word)
+        else:
+            sort_char = word[index]
+            buckets[order_map[sort_char]].append(word)
 
+    for bucket in buckets:
+        to_return += lex_sort(bucket, order, index +1)
+    return to_return
--- test_lex.py
+++ test_lex.py
@@ -3,11 +3,27 @@ from lex import lex_sort
 
 class TestStringMethods(unittest.TestCase):
 
-  def test_returns_all_items(self):
-      test1 = lex_sort(['abc','def'], 'abc')
-      self.assertEqual(len(test1), 2)
-      self.assertTrue('abc' in test1)
-      self.assertTrue('def' in test1)
+    def test_returns_all_items(self):
+        test1 = lex_sort(['abc','def'], 'abcdef')
+        self.assertEqual(len(test1), 2)
+        self.assertTrue('abc' in test1)
+        self.assertTrue('def' in test1)
+
+    def test_sorts_equal(self):
+        test1 =   lex_sort(["acb", "abc", "bca"], "abc")
+        self.assertEqual(len(test1), 3)
+        self.assertEqual(test1[0], "abc")
+        self.assertEqual(test1[1], "acb")
+        self.assertEqual(test1[2], "bca")
+
+    def test_sorts_same_char(self):
+        test1 = lex_sort(["a", "aaa","", "aa"], "a")
+        self.assertEqual(len(test1), 4)
+        self.assertEqual(test1[0], "")
+        self.assertEqual(test1[1], "a")
+        self.assertEqual(test1[2], "aa")
+        self.assertEqual(test1[3], "aaa")
+
 
 
 if __name__ == '__main__':
